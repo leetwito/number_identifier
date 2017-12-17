306a88421d5fc476d22f5bf67ceb2456fe5678de
student-1 <ran.feldesh@gmail.com>
2017-12-17 14:56:49 +0200

    Threshold,  Orientation, Segmentation, Resize

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -4,9 +4,11 @@ import sklearn
 
 img = cv2.imread('big-Num1.jpg', 0)
 ret, thresh = cv2.threshold(img, 40, 255, cv2.THRESH_BINARY)  # ToDo - set threshold (40) automatically
-print type(thresh[0, 0])
 thresh = abs(thresh.astype(float) - 255)
 thresh = thresh.astype('uint8')
+cv2.imshow("thresh", thresh)
+cv2.waitKey(0)
+
 
 # grab the (x, y) coordinates of all pixel values that
 # are greater than zero, then use these coordinates to
@@ -39,20 +41,35 @@ rotated = (rotated > 127).astype('uint8') * 255
 # *** SEGMENTATION ***
 
 ret, markers = cv2.connectedComponents(rotated, connectivity=8)
-print markers
-print np.max(markers)
-print markers.shape
-print np.unique(rotated)
-print type(markers[0, 0])
 
 rotated = (rotated > 127).astype('uint8') * 255
-
+data_mat = np.zeros((len(np.unique(markers))-1, 28*28))
 for i in np.unique(markers)[1:]:
     marker = (markers == i).astype('uint8') * 255
-    cv2.imshow("marker" + str(i), marker)
+    _, contours, hierarchy = cv2.findContours(marker, 2, 2)
+    cnt = contours[0]
+    x, y, w, h = cv2.boundingRect(cnt)
+    #cv2.rectangle(marker, (x, y), (x + w, y + h), (70, 255, 0))
+
+    square_edge = np.maximum(h, w)
+    square_edge = int(square_edge * 1.4)
+    top_bottom_padding = ((square_edge-h)/2, (square_edge-h)/2)
+    right_left_padding = ((square_edge-w)/2, (square_edge-w)/2)
+    only_digit = marker[y:y+h, x:x+w]
+    only_digit = np.pad(only_digit, [top_bottom_padding, right_left_padding], mode='constant')
+    resized_image = cv2.resize(only_digit, (28, 28), interpolation = cv2.INTER_AREA)
+    resized_image = (resized_image > 40).astype('uint8') * 255
+    reshaped_image = np.reshape(resized_image, (1,28*28))
+    data_mat[i-1,:] = reshaped_image.astype('int32')
+    # resized_image = only_digit
+    cv2.imshow("marker" + str(i), resized_image)
     cv2.waitKey(0)
 
+# cv2.imshow(" kaka", data_mat)
+# cv2.waitKey(0)
+# print data_mat.shape, np.max(data_mat), np.min(data_mat)
 # show the output image
 # cv2.imshow("Rotated Segmentation", rotated)
-cv2.imshow("rotated", rotated)
+#cv2.imshow("rotated", rotated)
 # cv2.imshow("Input", thresh)
+global data_mat
